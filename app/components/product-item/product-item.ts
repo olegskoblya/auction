4dd390ebedc05dee17bd73237773c45f1f4adef2
OSkoblya <oleg.skoblya@meta.ua>
2016-12-14 10:42:40 +0200

    complete exercise

---
+++ app/components/product-item/product-item.ts
@@ -0,0 +1,15 @@
+/**
+ * Created by OSkoblya on 13.12.2016.
+ */
+
+import {Component, Input} from '@angular/core';
+import StarsComponent from '../stars/stars';
+import {Product} from '../../services/product-service';
+
+@Component({
+    selector: 'auction-product-item',
+    templateUrl: 'app/components/product-item/product-item.html'
+})
+export default class ProductItemComponent {
+    @Input() product: Product;
+}
