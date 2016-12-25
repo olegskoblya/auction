c7bde53bcecc2d07d3a9198616adae0380b382f2
oskoblya <oskoblya@at-consulting.ru>
2016-12-25 14:45:48 +0200

    addition

--- app/components/application/application.ts
+++ app/components/application/application.ts
@@ -3,7 +3,6 @@
  */
 
 import {Component, ViewEncapsulation} from '@angular/core';
-import {Product, ProductService} from '../../services/product-service';
 
 @Component({
     selector: 'auction-application',
@@ -11,10 +10,4 @@ import {Product, ProductService} from '../../services/product-service';
     styleUrls: ['app/components/application/application.css'],
     encapsulation: ViewEncapsulation.None
 })
-export default class ApplicationComponent {
-    products: Array<Product> = [];
-
-    constructor(private productService: ProductService) {
-        this.products = this.productService.getProducts();
-    }
-}
+export default class ApplicationComponent {}
--- app/components/product-item/product-item.ts
+++ app/components/product-item/product-item.ts
@@ -3,11 +3,11 @@
  */
 
 import {Component, Input} from '@angular/core';
-import StarsComponent from '../stars/stars';
 import {Product} from '../../services/product-service';
 
 @Component({
     selector: 'auction-product-item',
+    styleUrls:['app/components/product-item/product-item.css'],
     templateUrl: 'app/components/product-item/product-item.html'
 })
 export default class ProductItemComponent {
